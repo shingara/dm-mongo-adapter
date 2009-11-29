f3dda8d9f0a31f86906507e4a35fadf29f845d4f
Piotr Solnica <piotr@zenbe.com>
2009-11-29 19:59:00 +0100

    removed embedded resource property type in favor of upcoming embedments API

--- lib/dm-mongo-adapter/types.rb
+++ lib/dm-mongo-adapter/types.rb
@@ -2,5 +2,4 @@ dir = Pathname(__FILE__).dirname.expand_path
 
 require dir / 'types' / 'object_id'
 require dir / 'types' / 'db_ref'
-require dir / 'types' / 'embedded_resource'
 require dir / 'types' / 'objects'
