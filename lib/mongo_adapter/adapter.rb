300d65d99da6a305d3fa63bbe1cb93b892a07d18
Lance Carlson <lancecarlson@gmail.com>
2010-01-07 21:55:38 -0500

    _id fields turned into ObjectID's

--- lib/mongo_adapter/adapter.rb
+++ lib/mongo_adapter/adapter.rb
@@ -84,11 +84,13 @@ module DataMapper
           end
 
         # make this prettier and off on its own method
-        attributes.each do |k, v| 
+        attributes.each do |k, v|
           if v.is_a?(Class) 
             attributes[k] = v.to_s
           elsif v.is_a?(DateTime) || v.is_a?(Date)
             attributes[k] = Time.parse(v.to_s)
+          elsif v.is_a?(String) && !k.grep(/_id$/).empty? && k != "_id"
+            attributes[k] = ::Mongo::ObjectID.from_string(v)
           end
         end
 
