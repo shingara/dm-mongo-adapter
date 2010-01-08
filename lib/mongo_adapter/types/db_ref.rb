290526fb64ca32b8b8c0ad6603537ba329b7ac77
Anthony Williams <anthony@ninecraft.com>
2010-01-09 03:45:20 +0800

    Add documentation for Types::DBRef and Types::ObjectID.

--- lib/mongo_adapter/types/db_ref.rb
+++ lib/mongo_adapter/types/db_ref.rb
@@ -1,13 +1,33 @@
 module DataMapper
   module Mongo
     module Types
+      # Database references are references from one document (object) to
+      # another within a database. A database reference is a standard embedded
+      # object: this is a MongoDB convention, not a special type.
+      #
+      # The DBRef is made available via your model as a String.
+      #
+      # @see http://www.mongodb.org/display/DOCS/DB+Ref
+      #
+      # @api public
       class DBRef < DataMapper::Type
         primitive ::Object
 
+        # Returns the DBRef as a string; suitable for use in a Resource
+        #
+        # @return [String]
+        #
+        # @api public
         def self.load(value, property)
           typecast(value, property)
         end
 
+        # Returns the DBRef as a Mongo ObjectID; suitable to be passed to the
+        # Mongo library
+        #
+        # @return [Mongo::ObjectID] The dumped ID.
+        #
+        # @api public
         def self.dump(value, property)
           case value
           when NilClass
@@ -21,6 +41,11 @@ module DataMapper
           end
         end
 
+        # Returns the DBRef as a string
+        #
+        # @return [String]
+        #
+        # @api public
         def self.typecast(value, property)
           case value
           when NilClass
@@ -33,6 +58,7 @@ module DataMapper
             raise ArgumentError.new('+value+ must be nil, String, ObjectID')
           end
         end
+
       end # DBRef
     end # Types
   end # Mongo
--- lib/mongo_adapter/types/object_id.rb
+++ lib/mongo_adapter/types/object_id.rb
@@ -1,16 +1,40 @@
 module DataMapper
   module Mongo
     module Types
+      # Each object (document) stored in Mongo DB has an _id field as its
+      # first attribute.  This is an object id.  It must be unique for each
+      # member of a collection (this is enforced if the collection has an _id
+      # index, which is the case by default).
+      #
+      # The database will assign an _id if an object being inserted into a
+      # collection does not have one.
+      #
+      # The _id may be of any type as long as it is a unique value.
+      #
+      # @see http://www.mongodb.org/display/DOCS/Object+IDs
+      #
+      # @api public
       class ObjectID < DataMapper::Type
         primitive ::Object
         key true
         field "_id"
         required false
 
+        # Returns the ObjectID as a string; suitable for use in a Resource
+        #
+        # @return [String]
+        #
+        # @api public
         def self.load(value, property)
           typecast(value, property)
         end
 
+        # Returns the ObjectID as a Mongo::ObjectID; suitable to be passed to
+        # the Mongo library
+        #
+        # @return [Mongo::ObjectID] The dumped ID.
+        #
+        # @api public
         def self.dump(value, property)
           case value
           when NilClass
@@ -24,6 +48,11 @@ module DataMapper
           end
         end
 
+        # Returns the ObjectID as a string
+        #
+        # @return [String]
+        #
+        # @api public
         def self.typecast(value, property)
           case value
           when NilClass
@@ -36,6 +65,7 @@ module DataMapper
             raise ArgumentError.new('+value+ must be nil, String or ObjectID')
           end
         end
+
       end # ObjectID
     end # Types
   end # Mongo
