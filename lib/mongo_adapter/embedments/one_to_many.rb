da6983c1a05e3394ded58b38b61eb9fa18298073
Anthony Williams <anthony@ninecraft.com>
2010-01-09 03:43:16 +0800

    Add documentation for Embedments::OneToMany.

--- lib/mongo_adapter/embedments/one_to_many.rb
+++ lib/mongo_adapter/embedments/one_to_many.rb
@@ -3,6 +3,13 @@ module DataMapper
     module Embedments
       module OneToMany
         class Relationship < Embedments::Relationship
+          # Loads and returns embedment target for given source
+          #
+          # @param [DataMapper::Mongo::Resource] source
+          #   The resource whose relationship value is to be retrieved.
+          #
+          # @return [DataMapper::Collection]
+          #
           # @api semipublic
           def get(source, other_query = nil)
             assert_kind_of 'source', source, source_model
@@ -14,6 +21,17 @@ module DataMapper
             get!(source)
           end
 
+          # Sets and returns association target for given source
+          #
+          # @param [DataMapper::Mongo::Resource] source
+          #   The parent resource whose target is to be set.
+          # @param [DataMapper::Mongo::EmbeddedResource] targets
+          #   The embedded resources to be set to the relationship
+          # @param [Boolean] loading
+          #   Do the attributes have to be loaded before being set? Setting
+          #   this to true will typecase the attributes, and set the
+          #   original_values on the resource.
+          #
           # @api semipublic
           def set(source, targets, loading=false)
             assert_kind_of 'source',  source,  source_model
@@ -32,25 +50,55 @@ module DataMapper
 
           private
 
+          # Creates a new collection instance for the source resources.
+          #
+          # @param [DataMapper::Mongo::Resource] source
+          #   The resources to be wrapped in a Collection.
+          #
+          # @return [DataMapper::Collection]
+          #
           # @api private
           def collection_for(source, other_query=nil)
             Collection.new(source)
           end
         end
 
+        # Extends Array to ensure that each EmbeddedResource has it's +parent+
+        # attribute set.
         class Collection < Array
+          # Returns the resource to which this collection belongs
+          #
+          # @return [DataMapper::Mongo::Resource]
+          #   The resource to which the contained EmbeddedResource instances
+          #   belong.
+          #
+          # @api semipublic
           attr_reader :source
 
+          # Creates a new Collection instance
+          #
+          # @param [DataMapper::Mongo::Resource] source
+          #   The resource to which the contained EmbeddedResource instances
+          #   belong.
+          #
+          # @api semipublic
           def initialize(source)
             @source = source
           end
 
+          # Adds a new embedded resource to the collection
+          #
+          # @param [DataMapper::Mongo::EmbeddedResource] resource
+          #   The embedded resource to be added.
+          #
+          # @api semipublic
           def <<(resource)
             resource.parent = source
             super(resource)
           end
         end
-      end
-    end
-  end
-end
+
+      end # OneToMany
+    end # Embedments
+  end # Mongo
+end # DataMapper
