5fe54a9c57037bba07cf352bf7f611ff3b1f8c81
Anthony Williams <anthony@ninecraft.com>
2010-01-10 17:03:38 +0000

    Fix old spec helper path so as to require the new helper only once.

--- spec/legacy/spec_helper.rb
+++ spec/legacy/spec_helper.rb
@@ -1,3 +1,3 @@
-require File.join(File.dirname(__FILE__), '..', 'spec_helper')
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 $db = Mongo::Connection.new.db('dm-mongo-test')
 include DataMapper::Mongo
