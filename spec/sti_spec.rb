ba572afb7756acf333d385b3a4f62d8ffc185d11
Piotr Solnica <piotr@zenbe.com>
2010-01-10 16:38:01 +0100

    Cleaned up sti specs a little

--- spec/sti_spec.rb
+++ spec/sti_spec.rb
@@ -2,9 +2,7 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe "Single Table Inheritance" do
   before(:all) do
-    $db.drop_collection('people')
-
-    class ::Person
+    class Person
       include DataMapper::Mongo::Resource
 
       property :id, ObjectID
@@ -13,43 +11,42 @@ describe "Single Table Inheritance" do
       property :type, Discriminator
     end
 
-    class ::Male < Person; end
-    class ::Father < Male; end
-    class ::Son < Male; end
+    class Male < Person; end
+    class Father < Male; end
+    class Son < Male; end
+  end
+
+  before(:each) do
+    $db.drop_collection('people')
   end
 
   it "should have a type property that reflects the class" do
-    [Person, Male, Father, Son].each do |model|
-      object = model.create
+    [Person, Male, Father, Son].each_with_index do |model, i|
+      object = model.create!(:name => "#{model} #{i}")
       object.reload
       object.type.should == model
     end
   end
 
   it "should parent should return an instance of the child when type is explicitly specified" do
-    [Person, Male, Father, Son].each do |model|
-      object = model.create
+    [Person, Male, Father, Son].each_with_index do |model, i|
+      object = model.create!(:name => "#{model} #{i}")
       object.reload
       object.should be_instance_of(model)
     end
   end
 
   it "should discriminate types during reads" do
-    $db.drop_collection('people')
-
-    father1 = Father.create
-    father2 = Father.create
-    son1 = Son.create
-    son2 = Son.create
+    father1 = Father.create!(:name => '1')
+    father2 = Father.create!(:name => '2')
 
     fathers = Father.all
+
     fathers.should == [father1, father2]
+
     fathers.each do |father|
+      father.type.should be(Father)
       father.should be_instance_of(Father)
     end
-
-    sons = Son.all
-    sons.should == [son1, son2]
   end
-
 end
