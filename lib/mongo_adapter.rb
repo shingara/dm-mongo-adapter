22fdbbe70294bad1778f4f1af2e4ede9f0030bbf
Anthony Williams <anthony@ninecraft.com>
2010-01-10 22:06:09 +0800

    Extract DM::M::Resource::ModelMethods out to DM::M::Model.
    This approach means we mixin Mongo-specific functionality only to
    resources which are associated with Mongo repositories. The previous
    approach was to override DM::Model#load which would have adverse side
    effects when loading resources belonging to other adapters.

--- lib/mongo_adapter.rb
+++ lib/mongo_adapter.rb
@@ -14,13 +14,16 @@ require dir / 'types' / 'discriminator'
 require dir / 'types' / 'object_id'
 require dir / 'types' / 'db_ref'
 require dir / 'types' / 'objects'
+
+require dir / 'model' / 'embedment'
+require dir / 'model'
+
 require dir / 'resource'
 
 require dir / 'embedments' / 'relationship'
 require dir / 'embedments' / 'one_to_one'
 require dir / 'embedments' / 'one_to_many'
 
-require dir / 'model' / 'embedment'
 require dir / 'embedded_model'
 require dir / 'embedded_resource'
 require dir / 'adapter'
--- lib/mongo_adapter/resource.rb
+++ lib/mongo_adapter/resource.rb
@@ -3,12 +3,14 @@ module DataMapper
     # Used in preference over DataMapper::Resource to add MongoDB-specific
     # functionality to models.
     module Resource
-      def self.included(base)
-        DataMapper::Model.append_extensions(ModelMethods)
+      def self.included(model)
+        model.send(:include, DataMapper::Resource)
+        model.send(:include, ResourceMethods)
+        model.send(:include, DataMapper::Mongo::Types)
 
-        base.send(:include, DataMapper::Resource)
-        base.send(:include, ResourceMethods)
-        base.send(:include, DataMapper::Mongo::Types)
+        # Needs to be after the inclusion of DM::Resource so as to overwrite
+        # methods added by DM::Model.
+        model.extend(Model)
       end
 
       module ResourceMethods
@@ -106,47 +108,6 @@ module DataMapper
           end
         end
       end # ResourceMethods
-
-      module ModelMethods
-        # Loads an instance of this Model, taking into account IdentityMap
-        # lookup, inheritance columns(s) and Property typecasting. Also loads
-        # the embedments on the Resource.
-        #
-        # @param [Enumerable<Object>] records
-        #   An Array of Resource or Hashes to load a Resource with
-        # @param [DataMapper::Query] query
-        #   The query used to load the Resource
-        #
-        # @return [Resource]
-        #   The loaded Resource instance
-        #
-        # @overrides DataMapper::Model#load
-        #
-        # @api semipublic
-        def load(records, query)
-          if discriminator = properties(query.repository.name).discriminator
-            records.each do |record|
-              discriminator_key   = discriminator.name.to_s
-              discriminator_value = discriminator.type.load(record[discriminator_key], discriminator)
-
-              record[discriminator_key] = discriminator_value
-            end
-          end
-
-          resources = super
-
-          # Load embedded resources
-          resources.each_with_index do |resource, index|
-            resource.model.embedments.each do |name, relationship|
-              unless (targets = records[index][name.to_s]).blank?
-                relationship.set(resource, targets, true)
-              end
-            end
-          end
-
-          resources
-        end
-      end # ModelMethods
 
     end # Resource
   end # Mongo
