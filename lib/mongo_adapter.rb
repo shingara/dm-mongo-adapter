7fdd012f63cc528a57d9e82537cb3474ba5e6ac9
Piotr Solnica <piotr@llp.pl>
2010-05-27 11:58:34 +0200

    No need to hack assert_valid_conditions on Query anymore

--- lib/mongo_adapter.rb
+++ lib/mongo_adapter.rb
@@ -25,37 +25,3 @@ require dir / 'modifier'
 
 require dir / 'aggregates'
 require dir / 'adapter'
-
-module DataMapper
-  module Mongo
-    module QueryExtensions
-      def self.included(base)
-        # FIXME: figure out a cleaner approach than AMC
-        base.class_eval <<-RUBY, __FILE__, __LINE__ + 1
-          alias assert_valid_conditions_without_embedment assert_valid_conditions
-          alias assert_valid_conditions assert_valid_conditions_with_embedment
-        RUBY
-      end
-
-      def assert_valid_conditions_with_embedment(conditions)
-        if conditions.is_a?(Hash) && model.respond_to?(:embedments) && !model.embedments.blank?
-          conditions.each_key do |key|
-            key_s = key.to_s
-
-            name  = if key_s.include?('.')
-              key_s.split('.')[0].to_sym
-            else
-              key
-            end
-
-            conditions.delete(key) if model.embedments.key?(name)
-          end
-        end
-
-        assert_valid_conditions_without_embedment(conditions)
-      end
-    end
-  end
-end
-
-DataMapper::Query.send(:include, DataMapper::Mongo::QueryExtensions)
