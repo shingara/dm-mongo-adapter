0cd242c6bc3037801704c8a5afecf680433aa5be
Piotr Solnica <piotr.solnica@gmail.com>
2010-10-16 11:33:25 +0200

    No need to override Model#property anymore

--- lib/mongo_adapter/model.rb
+++ lib/mongo_adapter/model.rb
@@ -1,36 +1,6 @@
 module DataMapper
   module Mongo
     module Model
-      # Defines a Property on the Resource
-      #
-      # Overrides the property method in dm-core so as to automatically map
-      # Array and Hash types to EmbeddedArray and EmbeddedHash respectively.
-      #
-      # @param [Symbol] name
-      #   the name for which to call this property
-      # @param [Type] type
-      #   the type to define this property ass
-      # @param [Hash(Symbol => String)] options
-      #   a hash of available options
-      #
-      # @return [Property]
-      #   the created Property
-      #
-      # @api public
-      def property(name, type, options = {})
-        if Array == type
-          type = DataMapper::Mongo::Property::Array
-        elsif Hash == type
-          type = DataMapper::Mongo::Property::Hash
-        elsif DateTime == type
-          type = DataMapper::Mongo::Types::DateTime
-        elsif Date == type
-          type = DataMapper::Mongo::Types::Date
-        end
-
-        super(name, type, options)
-      end
-
       private
 
       # @api private
