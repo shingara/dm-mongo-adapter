4c3a11602ee97f5d1cf7faa6bdc83200eb4b1827
Piotr Solnica <piotr.solnica@gmail.com>
2011-03-12 12:31:38 +0100

    Updated model specs

--- spec/public/model_spec.rb
+++ spec/public/model_spec.rb
@@ -1,37 +1,23 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 
 describe DataMapper::Mongo::Model do
-
   before(:all) do
     class ::PropertyTest
       include DataMapper::Mongo::Resource
       property :array_attr, Array
       property :hash_attr,  Hash
-      property :date,       Date
-      property :date_time,  DateTime
     end
   end
 
   describe '#property' do
-    it 'should cast Array types to EmbeddedArray' do
+    it 'should cast Array class to DataMapper::Mongo::Property::Array' do
       prop = PropertyTest.properties[:array_attr]
-      prop.type.should == DataMapper::Mongo::Types::EmbeddedArray
+      prop.should be_kind_of(DataMapper::Mongo::Property::Array)
     end
 
-    it 'should cast Hash types to EmbeddedHash' do
+    it 'should cast Hash class to DataMapper::Mongo::Property::Hash' do
       prop = PropertyTest.properties[:hash_attr]
-      prop.type.should == DataMapper::Mongo::Types::EmbeddedHash
-    end
-
-    it 'should cast Hash types to Types::Date' do
-      prop = PropertyTest.properties[:date]
-      prop.type.should == DataMapper::Mongo::Types::Date
-    end
-
-    it 'should cast Hash types to Types::DateTime' do
-      prop = PropertyTest.properties[:date_time]
-      prop.type.should == DataMapper::Mongo::Types::DateTime
+      prop.should be_kind_of(DataMapper::Mongo::Property::Hash)
     end
   end
-
 end
