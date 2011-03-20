e23be25c4c9c3f23f6c01c61ddff627ba3ccff38
Dan Kubb <dan.kubb@gmail.com>
2011-03-21 00:46:45 +0800

    Preserve fractional seconds from DateTime object

--- lib/mongo_adapter/support/date_time.rb
+++ lib/mongo_adapter/support/date_time.rb
@@ -1,8 +1,11 @@
 class DateTime
+  SEC_FRACTION_TO_USEC = 10**6 * (RUBY_VERSION < '1.9' ? 60 * 60 * 24 : 1)
+
   # @api public
   def self.to_mongo(value)
-    utc = value.new_offset(0)
-    ::Time.utc(utc.year, utc.month, utc.day, utc.hour, utc.min, utc.sec)
+    utc  = value.new_offset(0)
+    usec = utc.sec_fraction * SEC_FRACTION_TO_USEC
+    ::Time.utc(utc.year, utc.month, utc.day, utc.hour, utc.min, utc.sec, usec)
   end
 
   # @api public
